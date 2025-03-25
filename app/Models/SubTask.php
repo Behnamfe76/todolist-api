831cd733c83becc4a8bbb40dd5be33f0e3dc98ed
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-03-25 20:31:50 +0330

    feat(tasks): add Task and SubTask models with migrations

---
+++ app/Models/SubTask.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\TaskTypeEnum;
+use App\Enums\TaskStatusEnum;
+use App\Enums\TaskPriorityEnum;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class SubTask extends Model
+{
+    protected $table = 'sub_tasks';
+
+    protected $fillable = ['uuid', 'user_id', 'task_id', 'title', 'description', 'type', 'status', 'priority'];
+
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function task(): BelongsTo
+    {
+        return $this->belongsTo(Task::class);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function casts(): array
+    {
+        return [
+            'status' => TaskStatusEnum::class,
+            'type' => TaskTypeEnum::class,
+            'priority' => TaskPriorityEnum::class
+        ];
+    }
+}
---
+++ app/Models/Task.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\TaskPriorityEnum;
+use App\Enums\TaskStatusEnum;
+use App\Enums\TaskTypeEnum;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
+
+class Task extends Model
+{
+    protected $table = 'tasks';
+
+    protected $fillable = ['uuid', 'user_id', 'title', 'description', 'type', 'status', 'priority'];
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function tasks(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(SubTask::class);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function casts(): array
+    {
+        return [
+            'status' => TaskStatusEnum::class,
+            'type' => TaskTypeEnum::class,
+            'priority' => TaskPriorityEnum::class
+        ];
+    }
+}
