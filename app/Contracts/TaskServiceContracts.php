ae163b090e58d559af8d451e5b7ece8d7ba6d9c3
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-03-26 11:24:26 +0330

    feat(service): add index method to TaskService for retrieving all tasks with pagination

--- app/Contracts/TaskServiceContracts.php
+++ app/Contracts/TaskServiceContracts.php
@@ -2,12 +2,20 @@
 
 namespace App\Contracts;
 
-use App\Data\TaskData;
-use App\Http\Requests\V1\TaskStoreRequest;
 use Exception;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Throwable;
+use App\Data\TaskData;
+use Illuminate\Http\Request;
+use App\Http\Requests\V1\TaskStoreRequest;
 
 interface TaskServiceContracts{
+
+    /**
+     * @param Request $request
+     * @return Throwable|Exception|LengthAwarePaginator
+     */
+    public function index(Request $request): Throwable|Exception|LengthAwarePaginator;
 
     /**
      * @param TaskStoreRequest $request
--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -6,6 +6,7 @@ use App\Contracts\TaskRepositoryContracts;
 use App\Contracts\TaskServiceContracts;
 use App\Data\TaskData;
 use Exception;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use Throwable;
@@ -14,6 +15,22 @@ class TaskService implements TaskServiceContracts{
     public function __construct(
         private readonly  TaskRepositoryContracts $taskRepository,
     ){}
+
+
+    /**
+     * @param $request
+     * @return Throwable|Exception|LengthAwarePaginator
+     */
+    public function index($request): Throwable|Exception|LengthAwarePaginator
+    {
+        try {
+            return $this->taskRepository->getAllTasks($request);
+        } catch (\Throwable $tr) {
+            Log::error($tr->getMessage());
+
+            return $tr;
+        }
+    }
 
     /**
      * @param $request
