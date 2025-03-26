8b49fbbe878c9105d8d5dc9a4fda43e5f3b512c4
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-03-26 18:50:53 +0330

    feat(request): create TaskUpdateRequest for validating task updates

---
+++ app/Http/Requests/V1/TaskUpdateRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TaskUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'string|required|max:255',
+            'description' => 'string|nullable|max:10240',
+            'date' => 'required|date',
+        ];
+    }
+}
