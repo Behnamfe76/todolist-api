06b1624be882d4f250c2255d54ebdfa7094ed50d
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-03-26 15:03:46 +0330

    feat(controller): add updatePriority method to TaskController for updating task priority

--- routes/api/tasks.php
+++ routes/api/tasks.php
@@ -9,5 +9,6 @@ Route::prefix('/tasks')->middleware('auth:sanctum')->group(function () {
     Route::get('/{task:uuid}', [TaskController::class, 'show']);
     Route::patch('/{task:uuid}/update-type', [TaskController::class, 'updateType']);
     Route::patch('/{task:uuid}/update-status', [TaskController::class, 'updateStatus']);
+    Route::patch('/{task:uuid}/update-priority', [TaskController::class, 'updatePriority']);
     Route::post('/', [TaskController::class, 'store']);
 });
