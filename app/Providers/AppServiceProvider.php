3b2f9227ba18da00562928b3b9b88d5e76817023
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-03-26 10:34:50 +0330

    feat(provider): add pagination macro to Laravel Collection

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,12 +2,14 @@
 
 namespace App\Providers;
 
-use App\Contracts\TaskRepositoryContracts;
-use App\Contracts\TaskServiceContracts;
-use App\Repositories\TaskRepository;
 use App\Services\TaskService;
-use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Collection;
+use App\Repositories\TaskRepository;
+use App\Contracts\TaskServiceContracts;
 use Illuminate\Support\ServiceProvider;
+use App\Contracts\TaskRepositoryContracts;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Auth\Notifications\ResetPassword;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,6 +29,30 @@ class AppServiceProvider extends ServiceProvider
     {
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        });
+
+        /**
+         * Paginate a standard Laravel Collection.
+         *
+         * @param int $perPage
+         * @param int $total
+         * @param int $page
+         * @param string $pageName
+         * @return array
+         */
+        Collection::macro('paginate', function ($perPage, $total = null, $page = null, $pageName = 'page'): LengthAwarePaginator {
+            $page = $page ?: LengthAwarePaginator::resolveCurrentPage($pageName);
+
+            return new LengthAwarePaginator(
+                $this->forPage($page, $perPage)->values(),
+                $total ?: $this->count(),
+                $perPage,
+                $page,
+                [
+                    'path' => LengthAwarePaginator::resolveCurrentPath(),
+                    'pageName' => $pageName,
+                ]
+            );
         });
     }
 }
