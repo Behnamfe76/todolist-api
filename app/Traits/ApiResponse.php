b542fce0fc58f379318e8ac3851449091d553389
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-03-26 14:26:53 +0330

    feat(trait): extend successResponse method to accept JsonResource for enhanced data handling

--- app/Traits/ApiResponse.php
+++ app/Traits/ApiResponse.php
@@ -5,17 +5,18 @@ namespace App\Traits;
 use Spatie\LaravelData\Data;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 trait ApiResponse
 {
 
     /**
-     * @param array|Data|LengthAwarePaginator $data
+     * @param array|Data|LengthAwarePaginator|JsonResource $data
      * @param string $message
      * @param int $code
      * @return JsonResponse
      */
-    public function successResponse(array|Data|LengthAwarePaginator $data, string $message, int $code): \Illuminate\Http\JsonResponse
+    public function successResponse(array|Data|LengthAwarePaginator|JsonResource $data, string $message, int $code): \Illuminate\Http\JsonResponse
     {
         return response()->json([
             'status' => 'success',
