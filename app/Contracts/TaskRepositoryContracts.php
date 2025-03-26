5ed371cee7ac3e7a7fffd8b45078974e4372619b
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-03-26 11:24:05 +0330

    feat(repository): implement pagination for retrieving all tasks

--- app/Contracts/TaskRepositoryContracts.php
+++ app/Contracts/TaskRepositoryContracts.php
@@ -2,12 +2,21 @@
 
 namespace App\Contracts;
 
-use App\Data\TaskData;
-use App\Models\Task;
 use Exception;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Throwable;
+use App\Models\Task;
+use App\Data\TaskData;
+use Illuminate\Http\Request;
 
 interface TaskRepositoryContracts{
+
+    /**
+     * @param Request $request
+     * @return Throwable|Exception|LengthAwarePaginator
+     */
+    public function getAllTasks(Request $request): Throwable|Exception|LengthAwarePaginator;
+
     /**
      * @param TaskData $data
      * @return Task|Throwable|Exception
--- app/Repositories/TaskRepository.php
+++ app/Repositories/TaskRepository.php
@@ -5,23 +5,48 @@ namespace App\Repositories;
 use App\Contracts\TaskRepositoryContracts;
 use App\Models\Task;
 use Exception;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Log;
 use Throwable;
 
-class TaskRepository implements TaskRepositoryContracts{
-
+class TaskRepository implements TaskRepositoryContracts
+{
     /**
-     * @param $data
-     * @return Task|Throwable|Exception
+     * @param $request
+     * @return Throwable|Exception|LengthAwarePaginator
      */
-    public function store($data): Task|Throwable|Exception{
+    public function getAllTasks($request): Throwable|Exception|LengthAwarePaginator
+    {
         try {
-            return Task::create($data->toArray());
-        }catch(Throwable $tr){
+            $user = $request->user();
+            return Task::select('uuid', 'title', 'description', 'is_completed', 'status', 'due_date')
+                ->where('user_id', $user->id)
+                ->withCount([
+                    'subTasks as sub_tasks',
+                    'subTasks as completed_sub_tasks' => function ($query) {
+                        $query->where('is_completed', true);
+                    }
+                ])
+                ->paginate(10);
+        } catch (\Throwable $tr) {
             Log::error($tr->getMessage());
 
             return $tr;
         }
     }
 
+    /**
+     * @param $data
+     * @return Task|Throwable|Exception
+     */
+    public function store($data): Task|Throwable|Exception
+    {
+        try {
+            return Task::create($data->toArray());
+        } catch (Throwable $tr) {
+            Log::error($tr->getMessage());
+
+            return $tr;
+        }
+    }
 }
