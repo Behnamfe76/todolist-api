6a4f8146be88291262a9cc70dd6d16f0ff844e48
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-03-27 19:52:23 +0330

    feat(repository): enhance getAllTasks method with advanced filtering options

--- app/Repositories/TaskRepository.php
+++ app/Repositories/TaskRepository.php
@@ -3,11 +3,14 @@
 namespace App\Repositories;
 
 use App\Contracts\TaskRepositoryContracts;
+use App\Enums\TaskStatusEnum;
 use App\Models\Task;
 use Exception;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Log;
 use Throwable;
+
+use function Pest\Laravel\json;
 
 class TaskRepository implements TaskRepositoryContracts
 {
@@ -19,20 +22,80 @@ class TaskRepository implements TaskRepositoryContracts
     {
         try {
             $user = $request->user();
-            return Task::when($request->has('query'), function($query) use ($request) {
+
+            Log::info(
+                Task::when($request->has('query'), function ($query) use ($request) {
+                    $input = $request->get('query');
+                    if ($input) {
+                        $query->where(function ($q) use ($input) {
+                            $q->where('title', 'LIKE', "%$input%")
+                                ->orWhere('description', 'LIKE', "%$input%");
+                        });
+                    }
+                })
+                    ->when($request->has('status'), function ($query) use ($request) {
+                        $input = $request->get('status');
+                        $status = TaskStatusEnum::tryFrom($input);
+                        if ($status) {
+                            $query->where('status', $status);
+                        }
+                    })
+                    ->when($request->has('completed'), function ($query) use ($request) {
+                        $input = match ($request->get('completed')) {
+                            'completed' => true,
+                            'not_completed' => false,
+                            default => 'all'
+                        };
+                        if ($input !== 'all') {
+                            $query->where('is_completed', $input);
+                        }
+                    })
+                    ->select('id', 'uuid', 'user_id', 'title', 'description', 'is_completed', 'status', 'due_date')
+                    ->where('user_id', $user->id)
+                    ->withCount([
+                        'subTasks as sub_tasks',
+                        'subTasks as completed_sub_tasks' => function ($query) {
+                            $query->where('is_completed', true);
+                        }
+                    ])->toSql()
+
+            );
+
+            return Task::when($request->has('query'), function ($query) use ($request) {
                 $input = $request->get('query');
-                $query->whereLike('title', "%$input%")
-                    ->orWhereLike('description', "%$input%");
+                if ($input) {
+                    $query->where(function ($q) use ($input) {
+                        $q->where('title', 'LIKE', "%$input%")
+                            ->orWhere('description', 'LIKE', "%$input%");
+                    });
+                }
             })
-            ->select('uuid', 'title', 'description', 'is_completed', 'status', 'due_date')
+                ->when($request->has('status'), function ($query) use ($request) {
+                    $input = $request->get('status');
+                    $status = TaskStatusEnum::tryFrom($input);
+                    if ($status) {
+                        $query->where('status', $status);
+                    }
+                })
+                ->when($request->has('completed'), function ($query) use ($request) {
+                    $input = match ($request->get('completed')) {
+                        'completed' => true,
+                        'not_completed' => false,
+                        default => 'all'
+                    };
+                    if ($input !== 'all') {
+                        $query->where('is_completed', $input);
+                    }
+                })
+                ->select('id', 'uuid', 'user_id', 'title', 'description', 'is_completed', 'status', 'due_date')
                 ->where('user_id', $user->id)
+                ->orderByDesc('user_id')
                 ->withCount([
                     'subTasks as sub_tasks',
                     'subTasks as completed_sub_tasks' => function ($query) {
                         $query->where('is_completed', true);
                     }
-                ])
-                ->paginate(9);
+                ])->paginate(9);
         } catch (\Throwable $tr) {
             Log::error($tr->getMessage());
 
