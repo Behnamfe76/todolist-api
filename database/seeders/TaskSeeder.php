79afd876b463d4494f8654a4fe202b6eafd32a4c
Behnam Fereydooni <101217538+Behnamfe76@users.noreply.github.com>
2025-03-27 18:06:36 +0330

    feat(database): enhance TaskSeeder to adjust task creation range and update completion status

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Enums\TaskStatusEnum;
 use App\Models\Task;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
@@ -17,10 +19,23 @@ class TaskSeeder extends Seeder
         $users = User::all();
 
         foreach ($users as $user) {
-            Task::factory(random_int(1, 15))->create([
+            Task::factory(random_int(10, 20))->create([
                 'user_id' => $user->id
             ]);
         }
 
+        Task::cursor()->each(function ($task) {
+            if($task->status === TaskStatusEnum::DONE || $task->status === TaskStatusEnum::EXPIRED){
+                $task->update([
+                    'is_completed' => true,
+                    'done_date' => Carbon::parse($task->due_date)->addDays(random_int(1, 8))
+                ]);
+            }else{
+                $task->update([
+                    'is_completed' => false,
+                    'done_date' => null
+                ]);
+            }
+        });
     }
 }
